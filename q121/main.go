7a32db4020bbeb145c01fd1913b723f9cc5360de
jiangjiang.xu <jiangjiang.xu@shopee.com>
2022-04-11 09:27:54 +0800

    q121增加滑动窗口解法

--- q121/main.go
+++ q121/main.go
@@ -25,19 +25,53 @@ func maxProfit(prices []int) int {
 			continue
 		}
 		if profit > maxProfit { //之前的理论+今天的价差理论
-			start = buyPos + 2 //i对应的是diff下标，对应到prices需要+1 ，价格是从第一天开始，不是从第0天开始，故需要再+1，所以这里+2
-			end = i + 2        //同上
+			start = buyPos
+			end = i
 			maxProfit = profit
 		}
 	}
-	fmt.Printf("第%d天买入，第%d天卖出，利润最大%d\n", start, end, maxProfit)
+	if maxProfit > 0 {
+		fmt.Printf("第%d天买入，第%d天卖出，利润最大%d\n", start+2, end+2, maxProfit) ////i对应的是diff下标，对应到prices需要+1 ，价格是从第一天开始，不是从第0天开始，故需要再+1，所以这里+2
+	}
 	return maxProfit
+}
 
+//maxProfitV2 滑动窗口解法
+func maxProfitV2(prices []int) int {
+	//买卖需在不同一天，若价格列表长度小于1，则利润为0
+	if len(prices) <= 1 {
+		return 0
+	}
+	var maxProfit int
+	var profit int
+	var left, right int
+	var start, end int //记录买卖的天数
+	for right < len(prices) {
+		profit = prices[right] - prices[left]
+		if profit < 0 { //利率小于0
+			left = right
+			right++
+			continue
+		}
+		if maxProfit < profit {
+			maxProfit = profit
+			//记录买入和卖出如期
+			start = left
+			end = right
+		}
+		right++
+	}
+	if maxProfit > 0 {
+		fmt.Printf("第%d天买入，第%d天卖出，利润最大%d\n", start+1, end+1, maxProfit)
+	}
+	return maxProfit
 }
 
 func main() {
 	prices := []int{7, 1, 5, 3, 6, 4}
 	maxProfit(prices)
+	maxProfitV2(prices)
 	prices = []int{7, 6, 4, 3, 1}
 	maxProfit(prices)
+	maxProfitV2(prices)
 }
